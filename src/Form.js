0659916e3372ee18d25decc3c520f4c320c4a4b0
miki <milkyas.dev@gmail.com>
2021-10-13 18:03:13 +0300

    added  form and useState on the input

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import AddIcon from '@mui/icons-material/Add';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Form() {
+    // const [todos, setTodos] = useState([])
+    const [input, setInput] = useState('')
+
     return (
-        <form>
-            <input type="text" />
-        </form>
+        <>
+            <form>
+                <input
+                    onChange={e =>setInput(e.target.value)}
+                    value={input}
+                    type="text"
+                 />
+                <IconButton>
+                    <AddIcon color="primary"/>
+                </IconButton>
+            </form>
+        </>
     )
 }
 
