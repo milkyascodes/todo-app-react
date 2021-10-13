8dd3d217f74e98c1b555031715a6b3711ab26340
miki <milkyas.dev@gmail.com>
2021-10-13 17:55:27 +0300

    added the form component and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import Form from './Form'
+import './App.css'
 
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className="container">
       <Typography variant="h4" align="center" className="title">Todo App</Typography>
       <Typography align="center" className="sub-title"> 🔥 React 🔥 </Typography> 
+
+      <Form/>
     </div>
   )
 }
---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function Form() {
+    return (
+        <form>
+            <input type="text" />
+        </form>
+    )
+}
+
+export default Form
