517fa2df9251a23b125f3afdb76387b4fc64d6c1
miki <milkyas.dev@gmail.com>
2021-10-13 17:51:18 +0300

    added a heading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Typography } from '@mui/material'
 import React from 'react'
+
 
 function App() {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="container">
+      <Typography variant="h4" align="center" className="title">Todo App</Typography>
+      <Typography align="center" className="sub-title"> 🔥 React 🔥 </Typography> 
     </div>
   )
 }
